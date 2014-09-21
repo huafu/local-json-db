9ce94ee2f339cc8812c46de676b90457c39cb5ac
Huafu Gandon <huafu.gandon@gmail.com>
2014-09-22 05:42:06 +0700

    Fixing the excluded paths in coffee-coverage registration

--- spec/loader-coverage.js
+++ spec/loader-coverage.js
@@ -2,7 +2,7 @@ if (process.env.COVERAGE) {
   require('coffee-coverage').register({
     path: 'relative',
     basePath: require('path').join(__dirname, '..'),
-    exclude: ['/spec', '/node_modules', '/.git', '/.idea'],
+    exclude: ['spec', 'node_modules', '.git', '.idea'],
     initAll: true
   });
 }
