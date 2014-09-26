1202a9720796543d0e8c3a68874c991fa14af8e5
Huafu Gandon <huafu.gandon@gmail.com>
2014-09-26 15:05:16 +0700

    Fixed the coverage exclusion

--- specs/loader.js
+++ specs/loader.js
@@ -2,7 +2,7 @@ if (process.env.COVERAGE) {
   require('coffee-coverage').register({
     path:     'relative',
     basePath: require('path').join(__dirname, '..'),
-    exclude:  ['/specs', '/node_modules', '/.git', '/.idea', '/lib', '/docs'],
+    exclude:  ['specs', 'node_modules', '.git', '.idea', 'lib', 'docs'],
     initAll:  true
   });
 }
